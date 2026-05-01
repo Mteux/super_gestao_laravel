e98736be499b9b8574c7578a25dff6e16924597e
Mteux <smateus2605@gmail.com>
2026-04-30 21:22:56 -0300

    Aprendendo Eloquent ORM

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -4,7 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+// fornecedors
+// fornecedores
 class Fornecedor extends Model
 {
     //
+    protected $table = 'fornecedores';
+
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
 }
