e24cd0436c529b7fd55d472d384a13b14ef77977
Mteux <smateus2605@gmail.com>
2026-07-04 15:13:02 -0300

    Ajustando o formulario, repopulando os dados com laravel

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -9,18 +9,24 @@ class ContatoController extends Controller
 {
     public function contato(Request $request)
     {
+        $motivo_contatos = [
+            '1' => 'Dúvida',
+            '2' => 'Elogio',
+            '3' => 'Reclamação',
+        ];
 
-        return view('site.contato', ['titulo' => 'Contato']);
+        return view('site.contato', ['titulo' => 'Contato', 'motivo_contatos' => $motivo_contatos]);
     }
 
     public function salvar(Request $request)
     {
         $request->validate([
-            'nome' => 'required',
+            'nome' => 'required|min:3|max:40',
             'telefone' => 'required',
             'email' => 'required',
             'motivo_contato' => 'required',
-            'mensagem' => 'required',
+            'mensagem' => 'required|max:2000',
+
         ]);
         // SiteContato()::create($request->all());
     }
