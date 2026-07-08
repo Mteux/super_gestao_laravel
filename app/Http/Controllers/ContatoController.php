07c97ca51c327041f4041d8b9341af59b914130f
Mteux <smateus2605@gmail.com>
2026-07-08 16:58:40 -0300

    tratando os erros apresentados no formulario

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -18,14 +18,26 @@ class ContatoController extends Controller
 
     public function salvar(Request $request)
     {
-        $request->validate([
-            'nome' => 'required|min:3|max:40|unique:site_contatos',
-            'telefone' => 'required',
-            'email' => 'email',
-            'motivo_contatos_id' => 'required',
-            'mensagem' => 'required|max:2000',
+        $request->validate(
+            [
+                'nome' => 'required|min:3|max:40|unique:site_contatos',
+                'telefone' => 'required',
+                'email' => 'email',
+                'motivo_contatos_id' => 'required',
+                'mensagem' => 'required|max:2000',
 
-        ]);
+            ],
+            [
+                'nome.min' => 'O campo nome precisa ter no minimo 3 caracteres',
+                'nome.max' => 'O campo nome precisa ter no maximo 40 caracteres',
+                'nome.unique' => 'O campo nome informado já esta em uso',
+                'motivo_contatos_id' => 'O campo deve ser preenchido',
+                'email.email' => 'O email informado não é valido',
+
+                'required' => 'O campo :attribute deve ser preenchido',
+                'mensagem.max' => 'A mensagem deve ter no maximo 2000 caracteres',
+            ]
+        );
         SiteContato::create($request->all());
 
         return redirect()->route('site.index');
