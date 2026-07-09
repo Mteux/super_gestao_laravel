3de2f1226f0997594ac1b7625c7983c85c38da20
Mteux <smateus2605@gmail.com>
2026-07-09 12:26:26 -0300

    Corrige testes

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,4 +7,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Rodar migrations no banco de teste
+        $this->artisan('migrate:fresh', [
+            '--env' => 'testing',
+        ]);
+    }
 }
