47b01264262b562083a6052a2fba080299890918
Mteux <smateus2605@gmail.com>
2026-07-09 22:10:41 -0300

    passando parametros com middleware

--- app/Http/Middleware/AutenticacaoMiddleware.php
+++ app/Http/Middleware/AutenticacaoMiddleware.php
@@ -13,9 +13,22 @@ class AutenticacaoMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
+    public function handle(Request $request, Closure $next, $metodo_autenticacao, $perfil): Response
     {
-        if (true) {
+        echo $metodo_autenticacao.' - '.$perfil.'<br>';
+        if ($metodo_autenticacao == 'padrao') {
+            echo 'Verificar o usuario e senha no banco de dados'.$perfil.'<br>';
+        }
+
+        if ($metodo_autenticacao == 'ldap') {
+            echo 'Verificar o usuario e senha no AD'.$perfil.'<br>';
+        }
+        if ($perfil == 'visitante') {
+            echo 'Exibir apenas alguns recursos';
+        } else {
+            echo 'Carregar o perfil do banco de dados';
+        }
+        if (false) {
             return $next($request);
         } else {
             return Response('Acesso negado: Rota exige autenticação');
