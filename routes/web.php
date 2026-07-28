271e6933ca48a08fc43299656f119013dd8b8bf0
Mteux <smateus2605@gmail.com>
2026-07-28 17:11:02 -0300

    Criando o formulario de consulta e adicionar fornecedores

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,12 @@ Route::middleware('autenticacao:padrao,visitante')->prefix('/app')->group(functi
     Route::get('/fornecedor', [\App\Http\Controllers\FornecedorController::class, 'index'])
         ->name('app.fornecedor');
 
+    Route::get('/fornecedor/adicionar', [\App\Http\Controllers\FornecedorController::class, 'adicionar'])
+        ->name('app.fornecedor.adicionar');
+
+    Route::post('/fornecedor/listar', [\App\Http\Controllers\FornecedorController::class, 'listar'])
+        ->name('app.fornecedor.listar');
+
     Route::get('/produto', [\App\Http\Controllers\ProdutoController::class, 'index'])
         ->name('app.produto');
 });
